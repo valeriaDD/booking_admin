d01e98d35021b88b2930d2f46ac4b3bfdcf7e7be
Valeria <valeria.dubina@isa.utm.md>
2024-05-06 15:17:36 +0300

    Add users page header

--- src/components/users/UserSearchFilterComponent.vue
+++ src/components/users/UserSearchFilterComponent.vue
@@ -1,17 +1,16 @@
 <template>
-  <div>
-    <el-input></el-input>
-    <div>
-      <el-radio-group size="large">
-        <el-radio-button label="All" value="All" />
-        <el-radio-button label="Internam" value="Internal" />
-        <el-radio-button label="External" value="External" />
-      </el-radio-group>
-    </div>
+  <div class="searcher">
+    <el-input placeholder="Search by keywords" type="text" class="searcher__item input flex-2">
+      <template #append>
+        <el-button class="button"><i class="icon-search" /></el-button>
+      </template>
+    </el-input>
+
     <el-select
       collapse-tags
       collapse-tags-tooltip
       placeholder="Select"
+      class="searcher__item input--select flex-1"
     >
       <el-option
         v-for="item in options"
@@ -20,9 +19,18 @@
         :value="item.value"
       />
     </el-select>
+
+    <div class="input--radio">
+      <el-radio-group size="large">
+        <el-radio-button label="All" value="All" />
+        <el-radio-button label="Internam" value="Internal" />
+        <el-radio-button label="External" value="External" />
+      </el-radio-group>
+    </div>
   </div>
 </template>
 <script>
+
 export default {
   name: 'UserSearchFilter',
   data() {
@@ -55,5 +63,27 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.searcher {
+  display: flex;
+  gap: 1rem;
 
+  .button {
+    i {
+      font-size: 1rem;
+      color: $--color-input-field;
+    }
+  }
+
+  &__item {
+    font-size: 1rem;
+  }
+}
+
+.flex-1 {
+  flex: 1;
+}
+
+.flex-2 {
+  flex: 2;
+}
 </style>
--- src/components/users/UsersComponent.vue
+++ src/components/users/UsersComponent.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="container">
+    <div class="container__header">
+      <h2 class="title">Users</h2>
+      <div class="subtitle">Mange your company users, including external ones</div>
+    </div>
     <user-search-filter class="searcher"></user-search-filter>
     <user-list-component></user-list-component>
   </div>
@@ -18,6 +22,22 @@ export default {
 <style scoped lang="scss">
 .container {
   padding: 2rem 4rem;
+
+  &__header {
+    color: $--color-primary;
+    margin-bottom: 3rem;
+
+    .title {
+      margin: 0;
+      font-size: 2rem;
+    }
+
+    .subtitle {
+      font-size: 0.95rem;
+      font-weight: 300;
+      color: $--color-indigo;
+    }
+  }
 
   .searcher {
     margin-bottom: 3rem;
