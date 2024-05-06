af6c51e9fdb78a638bfc265acf20491ec3964539
Valeria <valeria.dubina@isa.utm.md>
2024-05-06 22:49:30 +0300

    create changing booking step mechanism

--- src/components/calendar/booking/AddBookingComponent.vue
+++ src/components/calendar/booking/AddBookingComponent.vue
@@ -6,44 +6,46 @@
     width="1000"
     class="dialog"
     :z-index="5000"
+    @close="dialogVisible = false"
+    @closed="resetForm"
   >
     <div>
-      <choose-resource-component></choose-resource-component>
-<!--    <el-form class="form" label-position="top">-->
-<!--      <el-form-item-->
-<!--        class="form__input"-->
-<!--        label="Resource"-->
-<!--        prop="resource">-->
-<!--        <el-select class="input&#45;&#45;select" placeholder="Select"></el-select>-->
-<!--      </el-form-item>-->
+      <choose-resource-component v-if="isFirstStep"></choose-resource-component>
+      <el-form v-else class="form" label-position="top">
+        <el-form-item
+          class="form__input"
+          label="Resource"
+          prop="resource">
+          <el-select class="input--select" placeholder="Select"></el-select>
+        </el-form-item>
 
-<!--      <el-form-item-->
-<!--        class="form__input"-->
-<!--        label="Title"-->
-<!--        prop="title">-->
-<!--        <el-input class="input"></el-input>-->
-<!--      </el-form-item>-->
+        <el-form-item
+          class="form__input"
+          label="Title"
+          prop="title">
+          <el-input class="input"></el-input>
+        </el-form-item>
 
-<!--      <el-form-item-->
-<!--        class="form__input"-->
-<!--        label="Participants"-->
-<!--        prop="participants">-->
-<!--        <el-select-->
-<!--          multiple-->
-<!--          collapse-tags-->
-<!--          collapse-tags-tooltip-->
-<!--          placeholder="Select"-->
-<!--          class="input&#45;&#45;select">-->
-<!--        </el-select>-->
-<!--      </el-form-item>-->
-<!--    </el-form>-->
+        <el-form-item
+          class="form__input"
+          label="Participants"
+          prop="participants">
+          <el-select
+            multiple
+            collapse-tags
+            collapse-tags-tooltip
+            placeholder="Select"
+            class="input--select">
+          </el-select>
+        </el-form-item>
+      </el-form>
     </div>
 
     <template #footer>
       <div class="dialog-footer">
         <el-button class="button" @click="dialogVisible = false">Cancel</el-button>
-        <el-button class="button button--confirm" @click="dialogVisible = false">
-          Next
+        <el-button class="button button--confirm" @click="goToNextStep">
+          {{buttonText}}
         </el-button>
       </div>
     </template>
@@ -59,19 +61,39 @@ export default {
   components: { ChooseResourceComponent },
   data() {
     return {
-      bookingStore: useBookingStore(),
-    };
+      bookingStore: useBookingStore()
+    }
   },
   computed: {
     dialogVisible: {
       get() {
-        return this.bookingStore.isBookingFormVisible;
+        return this.bookingStore.isBookingFormVisible
       },
       set(newValue) {
-        this.bookingStore.setVisibility(newValue);
+        this.bookingStore.setVisibility(newValue)
       }
     },
+    isFirstStep() {
+      return this.bookingStore.isFirstStep
+    },
+    buttonText() {
+      if (this.isFirstStep) {
+        return 'Next step'
+      }
+
+      return 'Book'
+    }
   },
+  methods: {
+    resetForm() {
+      this.bookingStore.resetForm();
+    },
+    goToNextStep() {
+      if (this.isFirstStep) {
+        this.bookingStore.form.resource_id = 1
+      }
+    }
+  }
 }
 </script>
 
--- src/store/booking.store.js
+++ src/store/booking.store.js
@@ -3,10 +3,21 @@ import { defineStore } from 'pinia'
 export const useBookingStore = defineStore('booking', {
   state: () => ({
     isBookingFormVisible: true,
+    form: {
+      resource_id: undefined,
+    }
   }),
+  getters: {
+    isFirstStep() {
+      return !this.form.resource_id;
+    }
+  },
   actions: {
     setVisibility(isVisible) {
       this.isBookingFormVisible = !!isVisible
+    },
+    resetForm() {
+      this.form.resource_id = undefined;
     }
   }
 })
